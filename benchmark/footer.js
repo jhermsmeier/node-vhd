5111f45f1f8181e2271a6cf5900690ffd1c9fead
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-12-02 18:43:54 +0100

    test(benchmark): Add VHD.Footer benchmarks

---
+++ benchmark/footer.js
@@ -0,0 +1,86 @@
+var VHD = require( '..' )
+var bench = require( 'nanobench' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+
+const ITERATIONS = 100000
+
+bench( `new VHD.Footer() ⨉ ${ITERATIONS}`, function( run ) {
+
+  var footer = new VHD.Footer()
+
+  run.start()
+
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    footer = new VHD.Footer()
+  }
+
+  run.end()
+
+})
+
+bench.skip( `VHD.Footer.parse( buffer ) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, 'data', 'footer.bin' )
+  var buffer = fs.readFileSync( filename )
+  var footer = new VHD.Footer()
+
+  run.start()
+
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    footer = VHD.Footer.parse( buffer )
+  }
+
+  run.end()
+
+})
+
+bench( `VHD.Footer#parse( buffer ) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, 'data', 'footer.bin' )
+  var buffer = fs.readFileSync( filename )
+  var footer = new VHD.Footer()
+
+  run.start()
+
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    footer.parse( buffer )
+  }
+
+  run.end()
+
+})
+
+bench( `VHD.Footer#write( buffer ) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, 'data', 'footer.bin' )
+  var buffer = fs.readFileSync( filename )
+  var footer = new VHD.Footer.parse( buffer )
+  var copy = Buffer.alloc( VHD.Footer.SIZE )
+
+  run.start()
+
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    footer.write( copy )
+  }
+
+  run.end()
+
+})
+
+bench( `VHD.Footer#write() ⨉ ${ITERATIONS}`, function( run ) {
+
+  var filename = path.join( __dirname, 'data', 'footer.bin' )
+  var buffer = fs.readFileSync( filename )
+  var footer = new VHD.Footer.parse( buffer )
+  var copy = Buffer.alloc( VHD.Footer.SIZE )
+
+  run.start()
+
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    copy = footer.write()
+  }
+
+  run.end()
+
+})
